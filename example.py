618596d7700a0798f8cfbc24d9c155d9d7a3b365
Sam Griffith <garfunkelnator@gmail.com>
2017-07-14 12:40:30 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 from mypyqr import qr_gen, qr_read
 
-qr_gen("Item #1", "$5.00", "You should really read this file I guess")
+qr_gen("Item #1", "$5.00", "You should really read this file")
 
 qr_read("Item #1.png")
 
