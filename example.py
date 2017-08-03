4f1fd45f453cad97aae3774cfbeaf8474149c84c
Sam Griffith <garfunkelnator@gmail.com>
2017-08-03 12:48:44 -0400

    Allowed user input

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 from mypyqr import qr_gen, qr_read
 
-qr_gen("Item #1", "$5.00", "You should really read this file")
+x = input("Enter Information Here: ")
+qr_gen(x)
 
-qr_read("Item #1.png")
+qr_read(x)
 
